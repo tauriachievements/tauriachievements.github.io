6ac49e5d98f7bb992e122455bbbfe35c54b4ad25
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2026-01-30 18:35:09 +0100

    feat: remove # from url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
-  withHashLocation,
   withInMemoryScrolling,
   withRouterConfig,
   withViewTransitions
@@ -23,8 +22,7 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'enabled'
       }),
       withEnabledBlockingInitialNavigation(),
-      withViewTransitions(),
-      withHashLocation()
+      withViewTransitions()
     ),
     IconSetService,
     provideAnimationsAsync(),
