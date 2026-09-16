257ce1404bc78886af5e232f9241f2ad4327a97f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2026-09-16 05:48:04 +0200

    feat: remove m+ timer

--- src/app/ladder.component.ts
+++ src/app/ladder.component.ts
@@ -21,7 +21,6 @@ import {
 import { LadderPageStore } from './ladder-page.store';
 import { LeaderboardTableComponent } from './leaderboard-table.component';
 import { MythicCountdownComponent } from './mythic-countdown.component';
-import { MythicPlusPreviewComponent } from './mythic-plus-preview.component';
 import { LadderFilterState, LadderSort } from './ladder.types';
 import { UpdateBarComponent } from './update-bar.component';
 
@@ -30,7 +29,7 @@ import { UpdateBarComponent } from './update-bar.component';
   templateUrl: './ladder.component.html',
   styleUrls: ['./ladder.component.scss'],
   standalone: true,
-  imports: [CommonModule, UpdateBarComponent, MythicCountdownComponent, MythicPlusPreviewComponent, FilterBarComponent, LeaderboardTableComponent, BackToTopButtonComponent],
+  imports: [CommonModule, UpdateBarComponent, MythicCountdownComponent, FilterBarComponent, LeaderboardTableComponent, BackToTopButtonComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [LadderPageStore]
 })
